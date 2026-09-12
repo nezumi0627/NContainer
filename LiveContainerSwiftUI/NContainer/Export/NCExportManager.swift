a4db9e574e0123e426f4dd59d155c8d374da63cd
nezumi0627 <103794002+nezumi0627@users.noreply.github.com>
2026-09-12 23:46:04 +0900

    Revert "feat: add NContainer extension foundations"
    This reverts commit 8aa08b9cfb4f17c656422ea56aa36a3d7d94e3db.

--- LiveContainerSwiftUI/NContainer/Export/NCExportManager.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-final class NCExportManager {
-    /// Keeps export policy in the NContainer layer while reusing LiveContainer's signer/archiver.
-    func exportIPA(bundleName: String, extraInfo: [String: Any] = [:]) throws -> URL {
-        try LCUtils.archiveIPA(withBundleName: bundleName, includingExtraInfoDict: extraInfo)
-    }
-}
